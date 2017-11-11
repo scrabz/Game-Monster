2d7bb4ac246b1c3f7e51f4689dd016c1f06575de
Unknown <poisonbanjo@gmail.com>
2017-11-10 19:07:18 -0500

    11/10 Push

---
+++ MasterGameStudioProject/Assets/_AbilityScripts/CrowAction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowAction : MonoBehaviour {
+	public Rigidbody thisRigid;
+	public GameObject collisionObject;
+	public Vector3 pushBackDir;
+	public bool flyAway = false;
+	// Use this for initialization
+	void Start () {
+		thisRigid = this.GetComponent<Rigidbody> ();
+		thisRigid.velocity = transform.forward * 15f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (flyAway) {
+			transform.Translate (0, 0.1f, 0);
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Solid") {
+			Destroy (this.gameObject);
+		}
+		if (col.gameObject.tag == "Player1" || col.gameObject.tag == "Player2" || col.gameObject.tag == "Player3" || col.gameObject.tag == "Player4"){
+			if (this.GetComponent<AttackAction>().teamNum != col.gameObject.GetComponent<PlayerState>().teamNum && !col.gameObject.GetComponent<PlayerMovement>().isRolling) {
+				col.gameObject.GetComponent<PlayerHealth> ().GetHit (this.GetComponent<AttackAction>().damage);
+				pushBackDir = this.GetComponent<AttackAction>().creator.transform.Find("RotationPoint").forward;
+				collisionObject = col.gameObject;
+				col.gameObject.GetComponent<PlayerState> ().Pushback (0.15f,thisRigid.velocity.normalized);
+				//Destroy (thisRigid);
+				flyAway = true;
+
+			}
+		}
+
+
+	}
+		
+}
---
+++ MasterGameStudioProject/Assets/_AbilityScripts/LeechAction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeechAction : MonoBehaviour {
+	public Rigidbody thisRigid;
+	public Vector3 pushBackDir;
+	public bool moving = true;
+	// Use this for initialization
+	void Start () {
+		thisRigid = this.GetComponent<Rigidbody> ();
+		thisRigid.velocity = transform.forward * 25f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (moving == false) {
+			thisRigid.isKinematic = true;
+			Material mat = transform.Find("LeechModel").GetComponent<Renderer>().material;
+			Color color = mat.color;
+			mat.color = new Color(color.r, color.g, color.b, color.a - (0.5f * Time.deltaTime));
+			if (mat.color.a <= 0) {
+				Destroy (this.gameObject);
+			}
+		}
+			
+
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Solid") {
+			thisRigid.velocity = Vector3.zero;
+			moving = false;
+		}
+
+
+
+		if (moving) {
+			if (col.gameObject.tag == "Player1" || col.gameObject.tag == "Player2" || col.gameObject.tag == "Player3" || col.gameObject.tag == "Player4") {
+				if (this.GetComponent<AttackAction> ().teamNum != col.gameObject.GetComponent<PlayerState> ().teamNum && !col.gameObject.GetComponent<PlayerMovement> ().isRolling && moving == true) {
+					Destroy (thisRigid);
+					this.transform.SetParent (col.gameObject.transform);
+					col.gameObject.GetComponent<PlayerHealth> ().GetHit (this.GetComponent<AttackAction> ().damage);
+					col.gameObject.GetComponent<PlayerState> ().InflictPoison (5f);
+//				pushBackDir = this.GetComponent<Rigidbody> ().velocity.normalized * 1.2f;
+//				col.GetComponent<CharacterController> ().Move (pushBackDir);
+					moving = false;
+				}
+			}
+		}
+	}
+}
