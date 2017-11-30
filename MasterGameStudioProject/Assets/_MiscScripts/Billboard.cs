e3266e525fb48571e82bfbcd5bdbac72346cceb1
Connor Kneeland <c.kneeland@outlook.com>
2017-11-30 13:33:53 -0500

    Minor Cave Fix and Billboard.cs

---
+++ MasterGameStudioProject/Assets/_MiscScripts/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+
+//    float lockPos = 0;
+
+    void Update()
+    {
+          transform.LookAt(Camera.main.transform.position, -Vector3.down);
+
+//          transform.rotation = Quaternion.Euler( 0, 0, 0);
+
+//        transform.position = new Vector3( 0, 0, 0);
+    }
+
+}
